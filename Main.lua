e165f523474df6c1c3d97a169ae32c13f193689c
Ntdi <me@ntdi.world>
2023-04-15 16:41:05 -0400

    Create Main.lua

---
+++ Main.lua
@@ -0,0 +1,5 @@
+local placeId = game.placeId;
+
+if placeId == 12781347095 then -- Color Hide and Seek
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/n-tdi/NtdiWorldScriptHub/main/ColorHideAndSeek"))()
+end
