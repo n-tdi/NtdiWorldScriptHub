d281f9dcdb981a122cb15827e9b61e073ef4d851
Ntdi <me@ntdi.world>
2023-04-15 21:49:25 -0400

    Create EverySecondPoint.lua

---
+++ EverySecondPoint.lua
@@ -0,0 +1,30 @@
+
+local Finity = loadstring(game:HttpGet("https://raw.githubusercontent.com/LocalSmail/Finity/main/Library"))()
+local Player = game.Players.LocalPlayer;
+
+local farmWins = false
+
+local FinityWindow = Finity.new("Ntdi World", true, nil, nil, false, nil) 
+    
+local AutoCategory = FinityWindow:Category("Auto")
+
+local AfkSector = AutoCategory:Sector("AFK")
+
+local AfkCheat = AfkSector:Cheat("Button", "AFK Farm", function ()
+    farmWins = not farmWins
+end, {})
+
+while true do
+    if farmWins then
+        Player.Character.Humanoid.RootPart.CFrame = game:GetService("Workspace").Wins.Stage10.CFrame
+        Player.Character.Humanoid:MoveTo(Vector3.new(-33.3600082, 1.7280035, 10360.124))
+        wait(0.5)
+        Player.Character.Humanoid:MoveTo(Vector3.new(-22.3600082, 1.7280035, 10360.124))
+        wait(0.5)
+        Player.Character.Humanoid:MoveTo(Vector3.new(-33.3600082, 1.7280035, 10360.124))
+        wait(0.5)
+        Player.Character.Humanoid:MoveTo(Vector3.new(-22.3600082, 1.7280035, 10360.124))
+    end
+
+    wait(0.1)
+end
