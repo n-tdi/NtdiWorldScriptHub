bdd1980335e7fcb720221483a79065976794b99e
Ntdi <me@ntdi.world>
2023-04-16 10:34:26 -0400

    Update ColorHideAndSeek.lua

--- ColorHideAndSeek.lua
+++ ColorHideAndSeek.lua
@@ -1,63 +1,88 @@
 -- 12781347095
-local Finity = loadstring(game:HttpGet("https://raw.githubusercontent.com/LocalSmail/Finity/main/Library"))()
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/bloodball/-back-ups-for-libs/main/Revenant", true))()
 local Player = game.Players.LocalPlayer;
 
-local teleportToRandomPlayers = false
+_G.teleportToHiders = false;
+_G.autoPlay = false;
 
-local FinityWindow = Finity.new("Ntdi World", true, nil, nil, false, nil) 
-    
-local MiscCategory = FinityWindow:Category("Misc")
+local Autofarm = Library:Window({
+  Text = "Autofarm"
+})
 
-local TeamSector = MiscCategory:Sector("Team")
-local SeakerTeamCheat = TeamSector:Cheat("Button", "Seeker Team", function ()
-    Player.Team = game.Teams.Seeker
-end, {})
-local HiderTeamCheat = TeamSector:Cheat("Button", "Hider Team", function ()
-    Player.Team = game.Teams.Hider
-end, {})
-local SpectatorTeaMCheat = TeamSector:Cheat("Button", "Spectate Team", function ()
-    Player.Team = game.Teams.Spectator
-end, {})
+local Hider = Library:Window({
+  Text = "Hider"
+})
 
-local HiderCategory = FinityWindow:Category("Hider")
+local Seeker = Library:Window({
+  Text = "Seeker"
+})
 
-local WinSector = HiderCategory:Sector("Win")
-local WinCheat = WinSector:Cheat("Button", "Teleport to Top", function()
-    Player.Character.Humanoid.RootPart.CFrame = game:GetService("Workspace").Wait.SpawnLocation.CFrame
-end, {})
+Autofarm:Toggle({
+  Text = "Autoplay",
+  Callback = function(bool)
+    _G.autoPlay = bool;
+  end
+})
 
+Autofarm:Keybind({
+  Text = "Toggle Library",
+  Default = Enum.KeyCode.F4,
+  Callback = function()
+      Library:Toggle()
+  end
+})
 
+Hider:Button({
+  Text = "Teleport to Top",
+  Callback = function()
+    teleportToSpawn()
+  end
+})
 
-local LegitSector = HiderCategory:Sector("Legit")
-
-local RespawnCheat = LegitSector:Cheat("Button", "Free Respawn", function()
+Hider:Button({
+  Text = "Free Respawn",
+  Callback = function()
     Player.Character.Humanoid.RootPart.CFrame = game:GetService("Workspace").RespawnLocation.CFrame
-end, {})
+  end
+})
 
-local HiderCategory = FinityWindow:Category("Seeker")
+Seeker:Button({
+  Text = "Teleport to random Hider",
+  Callback = function()
+    teleportToRandomHider()
+  end
+})
 
-local WinSSector = HiderCategory:Sector("Win")
-local WinCCheat = WinSSector:Cheat("Button", "Teleport to random Hider", function()
-    local randomPlayer = game.Players:GetPlayers()[math.random(1,#game.Players:GetPlayers())]
+Seeker:Toggle({
+  Text = "Auto Win",
+  Callback = function(bool)
+    _G.teleportToHiders = bool
+  end
+})
+
+function teleportToSpawn()
+  Player.Character.Humanoid.RootPart.CFrame = game:GetService("Workspace").Wait.SpawnLocation.CFrame
+end
+
+function teleportToRandomHider()
+  local randomPlayer = game.Players:GetPlayers()[math.random(1,#game.Players:GetPlayers())]
 
     if randomPlayer.Team == game:GetService("Teams")["Hider"] then
-        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(Vector3.new(randomPlayer.Character.Head.Position.X, randomPlayer.Character.Head.Position.Y, randomPlayer.Character.Head.Position.Z))
-    end   
-end, {})
-
-local CycleWinCheat = WinSSector:Cheat("Button", "Cycle through Hiders", function()
-    teleportToRandomPlayers = not teleportToRandomPlayers
-    print(teleportToRandomPlayers)
-end, {})
-
-while true do
-    if teleportToRandomPlayers then
-       print("running")
-        local randomPlayer = game.Players:GetPlayers()[math.random(1,#game.Players:GetPlayers())]
-         
-        if randomPlayer.Team == game:GetService("Teams")["Hider"] then
-            game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(Vector3.new(randomPlayer.Character.Head.Position.X, randomPlayer.Character.Head.Position.Y, randomPlayer.Character.Head.Position.Z))
-        end
+        Player.Character.HumanoidRootPart.CFrame = CFrame.new(Vector3.new(randomPlayer.Character.Head.Position.X, randomPlayer.Character.Head.Position.Y, randomPlayer.Character.Head.Position.Z))
     end
-    wait(0.1) 
+end
+
+while wait() do
+  if _G.teleportToHiders then
+    teleportToRandomHider()
+  end
+
+  if _G.autoPlay then
+    if Player.Team == game:GetService("Teams")["Hider"] then
+      teleportToSpawn()
+      wait(10)
+    elseif Player.Team == game:GetService("Teams")["Seeker"] then
+      teleportToRandomHider()
+    end
+  end
 end
