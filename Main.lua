7f8729c6cc540213aef51b02b0dfb13b008f593f
Ntdi <me@ntdi.world>
2023-04-16 19:47:52 -0400

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -3,5 +3,7 @@ local placeId = game.placeId;
 if placeId == 12781347095 then -- Color Hide and Seek
   loadstring(game:HttpGet("https://raw.githubusercontent.com/n-tdi/NtdiWorldScriptHub/main/ColorHideAndSeek.lua"))()
 elseif placeId == 12114398020 then -- Every second gain point
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/n-tdi/NtdiWorldScriptHub/main/EverySecondPoint.lua"))()
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/n-tdi/NtdiWorldScriptHub/main/EverySecondPoint.lua"))()
+elseif placeId == 12398176076 then -- start an emo band from your garage tycoon
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/n-tdi/NtdiWorldScriptHub/main/EmoBand.lua"))()
 end
