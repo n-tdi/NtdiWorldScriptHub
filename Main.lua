09a361887880e5ef65919396fb13deb5b5f1f581
Ntdi <me@ntdi.world>
2023-04-15 21:50:21 -0400

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -2,4 +2,6 @@ local placeId = game.placeId;
 
 if placeId == 12781347095 then -- Color Hide and Seek
   loadstring(game:HttpGet("https://raw.githubusercontent.com/n-tdi/NtdiWorldScriptHub/main/ColorHideAndSeek.lua"))()
+else if placeId == 12114398020 then -- Every second gain point
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/n-tdi/NtdiWorldScriptHub/main/EverySecondPoint.lua"))()
 end
