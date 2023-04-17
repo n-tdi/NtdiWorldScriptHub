08bc959b20fd889c84082d66c49553f077c25dde
Ntdi <me@ntdi.world>
2023-04-16 22:53:27 -0400

    Update EmoBand.lua

--- EmoBand.lua
+++ EmoBand.lua
@@ -45,6 +45,8 @@ Auto:Keybind({
   end
 })
 
+loadstring(game:HttpGet("https://raw.githubusercontent.com/n-tdi/NtdiWorldScriptHub/main/Universal.lua"))()
+
 while wait() do
     if _G.AutoRestock then
         teleportToAndClick(coffeMaker)
