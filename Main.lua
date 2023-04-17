8a8c8260deba7452a6eaf36320236998305b02a8
Ntdi <me@ntdi.world>
2023-04-16 23:01:21 -0400

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -6,6 +6,4 @@ elseif placeId == 12114398020 then -- Every second gain point
   loadstring(game:HttpGet("https://raw.githubusercontent.com/n-tdi/NtdiWorldScriptHub/main/EverySecondPoint.lua"))()
 elseif placeId == 12398176076 then -- start an emo band from your garage tycoon
   loadstring(game:HttpGet("https://raw.githubusercontent.com/n-tdi/NtdiWorldScriptHub/main/EmoBand.lua"))()
-else
-  loadstring(game:HttpGet("https://raw.githubusercontent.com/n-tdi/NtdiWorldScriptHub/main/Universal.lua"))()
 end
